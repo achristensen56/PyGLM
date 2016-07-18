20bfa2560333957c66a7a05aca3ee58454aa2158
Amy <amyjc@stanford.edu>
2016-07-18 01:00:14 -0400

    First commit

---
+++ glm_model.py
@@ -0,0 +1,5 @@
+import numpy as np
+import tensorflow as tf
+
+class glm_model():
+	def __init__(non_linearity = tf.exp, noise_model = tf.):
---
+++ glm_utils.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def gamma():
+	return None
