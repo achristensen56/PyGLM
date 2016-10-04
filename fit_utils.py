8392a7dc55bcfb5cb8f06a5fa2dee94baaebcea5
achristensen56 <chandra.christensen@gmail.com>
2016-10-04 10:12:37 -0700

    could this work

--- fit_utils.py
+++ fit_utils.py
@@ -8,8 +8,8 @@ import pickle
 import allensdk.brain_observatory.stimulus_info as stim_info
 import itertools as it
 
-def fit_glm_CV(dff_array, design_matrix, non_linearity = [tf.exp, tf.nn.sigmoid, tf.nn.relu], 
-	noise_model = ['exponential', 'gaussian']):
+def fit_glm_CV(dff_array, design_matrix, learning_rate = [0.001], non_linearity = [tf.exp, tf.nn.sigmoid], batch_size = 5000,
+	noise_model = ['exponential', 'gaussian', 'lognormal'], num_pcs = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 118], debug = False, max_iters = 2000):
 	'''
 	dff_array (numpy array) = n_time_bins x n_cells
 	design_matrx (numpy array) = n_time_bins n_features
@@ -33,71 +33,117 @@ def fit_glm_CV(dff_array, design_matrix, non_linearity = [tf.exp, tf.nn.sigmoid,
 
 	#print 'the shape of the dff array is: ', dff_array.shape, 'the shape of the design matrix is: ', design_matrix.shape
 
-	n_nl, n_nm = len(non_linearity), len(noise_model)
+	n_nl, n_nm, n_pcs, n_lrs = len(non_linearity), len(noise_model), len(num_pcs), len(learning_rate)
 
-	scores = np.zeros([n_cells, n_nl, n_nm])
-	features = np.zeros([n_cells, n_nl, n_nm, n_features + 2]) 
+	scores = np.zeros([n_cells, n_nl, n_nm, n_pcs, n_lrs])
+	features = np.zeros([n_cells, n_nl, n_nm, n_pcs, n_lrs, n_features + 3]) 
 
-	nm_dict = {'exponential': gm.exponential_GLM, 'gaussian': gm.gaussian_GLM}
-	b = pyprind.ProgBar(len(noise_model)* len(non_linearity))
+	nm_dict = {'exponential': gm.exponential_GLM, 'gaussian': gm.gaussian_GLM, 'lognormal': gm.lognormal_GLM}
+	b = pyprind.ProgBar(len(noise_model)* len(non_linearity)*len(num_pcs))
+
 
 	for j, nm in enumerate(noise_model):
 		for k, nl in enumerate(non_linearity):
+			for m, npcs in enumerate(num_pcs):
+				for n, lr in enumerate(learning_rate):
 
-			b.update()
-			
-			tp = True
-			lr = 1e-5
+					b.update()
+					
+					tp = True
+					#lr = 1e-3
 
-			if nl == tf.exp or nl == tf.nn.relu:
-				#the exponential non-linearity 
-				tp = False
-				lr = 1e-6
-
-
-			#here we fit 5 different versions of the model, on different test sets, and average them together
-			test_l = []
-
-			weights = []
-			offset = []
-			scale = []
-
-			for i in range(5):
-
-				X_train, X_test, y_train, y_test = train_test_split(design_matrix, dff_array)
-
-				weight_init = np.linalg.pinv(X_train).dot(y_train) 
-
-				model = nm_dict[nm](weight_init,
-					lr = lr, train_params = tp, alpha = 0, reg = '', non_lin = nl, verbose = False)
-
-				L, l = model.fit(X_train, y_train, X_test, y_test, max_iters = 200, batch_size = 6000)
-				
-
-				w, o, s = model.get_params()	
-
-				l = np.array(l)
-				w = np.array(w)
-
-				weights.append(w)
-				offset.append(o)
-				scale.append(s)
-				test_l.append(np.squeeze(l[-1]))
+					if nl == tf.exp or nl == tf_soft_rec:
+						#the exponential non-linearity 
+						tp = False
+						#lr = 1e-3
 
 
-			#the parameters
-			w = np.mean(np.array(weights).reshape(5, n_cells, n_features), axis = 0)
-			o = np.mean(offset)
-			s = np.mean(scale)
-			l = np.mean(np.array(test_l), axis = 0)
 
-			#the average of the final test likelihood is what we actually use for model comparison
+					#here we fit 5 different versions of the model, on different test sets, and average them together
+					test_l = []
 
-			scores[:, k, j] = l
-			features[:, k, j, 0:-2] = w
-			features[:, k, j, -2] = o
-			features[:, k, j, -1] = s
-			
+					weights = []
+					offset = []
+					scale = []
+					variance = []
+
+					print "Fitting: ", nm, " noise model with: ", nl, " non-linearity, and: " , npcs ,"features"
+
+					for i in range(5):
+
+						X_train, X_test, y_train, y_test = train_test_split(design_matrix[:, 0:npcs], dff_array)
+
+						weight_init = np.linalg.pinv(X_train).dot(y_train) 
+
+						model = nm_dict[nm](weight_init,
+							lr = lr, train_params = tp, alpha = 0, reg = '', non_lin = nl, verbose = False)
+
+						L, l = model.fit(X_train, y_train, X_test, y_test, max_iters = max_iters, batch_size = batch_size)
+						
+
+						if debug == True:
+
+							plt.plot(L)
+							plt.show()
+
+							plt.plot(l)
+							plt.show()
+
+							if (~np.isfinite(l)).any():
+								print "The test likelihood is NAN"
+
+
+						w, o, s = model.get_params()	
+
+						l = np.array(l)
+						L = np.array(L)
+
+						w = np.array(w)
+
+						weights.append(w)
+						offset.append(o)
+						scale.append(s)
+						N = float(len(y_test))
+						A = np.squeeze(l[-1, :])
+
+
+						if nm == 'gaussian':
+
+							var = gaussian_variance(y_train, X_train, w, o, s)
+
+							variance.append(var)
+
+							l = 1/N * (-N/2 * np.log(2*np.pi) - N/2 * np.log(var) - 1 / (2*var) * A)
+						else:
+							l = -A / N
+
+						
+
+						test_l.append(l)
+
+					weights = np.array(weights)
+
+					#the parameters
+					w = np.mean(np.array(weights), axis = 0).T
+
+					plt.plot(w.T)
+
+					o = np.mean(offset, axis = 0)
+					s = np.mean(scale, axis = 0)
+					if 'nm' == 'gaussian':
+						v = np.mean(variance, axis =0)
+						features[:,k,j,m,n,n, -3] = v
+
+					l = np.mean(np.array(test_l), axis = 0)
+
+					#the average of the final test likelihood is what we actually use for model comparison
+
+					scores[:, k, j, m, n] = l
+					features[:, k, j, m, n, 0:npcs] = w
+
+					features[:, k, j, m, n,-2] = o
+					features[:, k, j, m, n, -1] = s
+		
 
 	return scores, features	 
 
@@ -106,8 +152,8 @@ def save_results_GLM((key, data_set)):
 	dff, images, stim_table = get_data(data_set, stim_info.NATURAL_SCENES)
 	r_images = pca_features(images)
 	data, stim_array = arrange_data_glm(dff, r_images, stim_table)
-	scores, features = fit_glm_CV(data, stim_array[:, 0:10])
-	output = open('./boc/formatted/' + str(key) + '10_CV_results.pkl', 'wb')
+	scores, features = fit_glm_CV(data, stim_array[:, 0:50])
+	output = open('./boc/formatted/' + str(key) + '50_CV_results.pkl', 'wb')
 	pickle.dump((scores, features), output)
 	output.close()
 
@@ -115,8 +161,8 @@ def save_results_GLM((key, data_set)):
 
 if __name__ == '__main__':
 		boc = BrainObservatoryCache(manifest_file='boc/manifest.json')
-		regions = ['VISl', 'VISp', 'VISpm', 'VISal']
-		lines = ['Cux2-CreERT2', 'Rbp4-Cre', 'Rorb-IRES2-Cre'] 
+		regions = ['VISpm']
+		lines = ['Cux2-CreERT2']#, 'Rbp4-Cre', 'Rorb-IRES2-Cre'] 
 
 		jobs = []
 
